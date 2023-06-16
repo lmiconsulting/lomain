25cd25dab81e510c1de635ccaf3b060cb8c9fb61
Lucas <lucashmccabe@gmail.com>
2023-06-16 15:47:15 -0400

    feat: readme and setup file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lomain
+
+`lomain` (after LOgistics MAnagement INstitute; read "lo mein"): a library of generic, well-worn utilities by LMI.
+
+![logo](./assets/lomein_astronaut.png)
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+DESCRIPTION = "generic, well-worn utilities"
+
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    LONG_DESCRIPTION = DESCRIPTION
+
+
+setup(
+    name="lomain",
+    version="0.0.1",
+    author="Lucas Hurley McCabe",
+    author_email="lmccabe@lmi.org",
+    packages=("lomain",),
+    url="https://github.com/lmiconsulting/lomain",
+    license="Apache-2.0 license",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=(
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache-2.0 license",
+        "Programming Language :: Python :: 3",
+    ),
+    zip_safe=True,
+)
