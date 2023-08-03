eca7cae0e835355a08855de69931fc93456f8d9b
= <=>
2023-08-02 22:53:58 -0400

    feat: checking os

--- lomain/runtime.py
+++ lomain/runtime.py
@@ -1,4 +1,5 @@
 import subprocess
+import platform
 
 def check_vpn(vpn_server_address=None):
     """Verifies if the user is connected to a VPN.
@@ -10,3 +11,12 @@ def check_vpn(vpn_server_address=None):
     """
     public_ip_address = subprocess.check_output(["curl", "ifconfig.co"]).decode("utf-8").strip()
     return public_ip_address == vpn_server_address
+
+def get_os():
+    """Returns whether the system is running Mac OSX
+    """
+    os = platform.system()
+    if os == "Darwin":
+        # using mac os
+        return True
+    return False
