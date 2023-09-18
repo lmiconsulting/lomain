c1ecd8c0ed5883c9653360c7fa5f0323afb01bb8
= <=>
2023-09-18 08:24:51 -0400

    feat: is_from_package

--- lomain/packages.py
+++ lomain/packages.py
@@ -1,4 +1,5 @@
 from importlib.metadata import version
+import inspect
 
 def get_version(pkg_name):
     """Returns the version number of a package.ß
@@ -9,3 +10,36 @@ def get_version(pkg_name):
         The name of the package to look up.
     """
     return version(pkg_name)
+
+def is_from_package(obj, package_name):
+    """Check if an object's class originates from a specific package.
+
+    Parameters
+    ----------
+    obj : object
+        The object to inspect.
+    package_name : str
+        Name of the package to check against.
+
+    Returns
+    -------
+    bool
+        True if the object's class is from the specified package, False otherwise.
+
+    Examples
+    --------
+    >>> a = np.array([1, 2, 3])
+    >>> is_from_package(a, "numpy")
+    True
+
+    Notes
+    -----
+    This function checks the immediate parent module of the object's class.
+    It may not accurately determine the package for deeply nested module structures.
+    """
+    # inspect the object's class and find its parent module
+    module_name = obj.__class__.__module__
+    split_module_name = module_name.rsplit('.', 1) if '.' in module_name else [module_name]
+    if package_name not in split_module_name:
+        return False
+    return True
